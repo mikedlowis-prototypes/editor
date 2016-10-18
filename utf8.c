3ce653bb742bb2f4ca5d59255fe35c3ec834e615
Mike Lowis <mike.lowis@gentex.com>
2016-10-18 08:23:47 -0400

    fixed compiler warning

--- utf8.c
+++ utf8.c
@@ -4,6 +4,7 @@
   @license BSD 2-clause License
 */
 #include "edit.h"
+#define __USE_XOPEN
 #include <wchar.h>
 
 const uint8_t UTF8_SeqBits[] = { 0x00u, 0x80u, 0xC0u, 0xE0u, 0xF0u, 0xF8u, 0xFCu, 0xFEu };
