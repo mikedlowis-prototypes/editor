f26ca5e35babece5c4b1960fe18bbb331af4c10d
Michael D. Lowis <mike.lowis@gentex.com>
2017-05-30 08:35:07 -0400

    Added column tracking to page up/dn

--- lib/view.c
+++ lib/view.c
@@ -573,12 +573,13 @@ void view_scrollpage(View* view, int move) {
     move = (move < 0 ? -1 : 1) * view->nrows;
     view_scroll(view, move);
     size_t off    = view->rows[view->nrows/2]->off;
-    size_t scrbeg = view->rows[0]->off;
-    size_t scrend = view->rows[view->nrows-1]->off + view->rows[view->nrows-1]->rlen - 1;
-    if (scrbeg == 0)
-        off = 0;
-    else if (scrend >= buf_end(&(view->buffer)))
-        off = view->rows[view->nrows-1]->off;
+
+//    size_t scrbeg = view->rows[0]->off;
+//    size_t scrend = view->rows[view->nrows-1]->off + view->rows[view->nrows-1]->rlen - 1;
+//    if (scrbeg == 0)
+//        off = 0;
+//    else if (scrend >= buf_end(&(view->buffer)))
+//        off = view->rows[view->nrows-1]->off;
 
 //    size_t off = (move == UP ? view->rows[0]->off : view->rows[view->nrows/2]->off);
 //    if (row != SIZE_MAX && col != SIZE_MAX) {
@@ -586,6 +587,12 @@ void view_scrollpage(View* view, int move) {
 //        if (col >= view->rows[row]->rlen)
 //            off = view->rows[row]->off + view->rows[row]->rlen - 1;
 //    }
+
+    if (row != SIZE_MAX && col != SIZE_MAX)
+        if (col >= view->rows[view->nrows/2]->rlen)
+            off = view->rows[view->nrows/2]->off + view->rows[view->nrows/2]->rlen - 1;
+        else
+            off += col;
 
     view_jumpto(view, false, off);
     view->sync_needed = false;
